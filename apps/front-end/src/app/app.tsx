de37f9fee7e3d7d8bba30c4497a94844bf2ed41d
Viktory <73409754+Veles-Pan@users.noreply.github.com>
2023-11-06 11:44:24 +0000

    style: linter

--- apps/front-end/src/app/app.tsx
+++ apps/front-end/src/app/app.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css'
-
 import NxWelcome from './nx-welcome'
+
+import styles from './app.module.css'
 
 export function App() {
   return (
--- apps/front-end/vite.config.ts
+++ apps/front-end/vite.config.ts
@@ -1,7 +1,8 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/front-end',
