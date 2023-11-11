8f7fdedd6b205a4187d59034ddd2ef35298b19c6
Viktory <73409754+Veles-Pan@users.noreply.github.com>
2023-11-11 23:03:55 +0000

    feat: prehistory page

--- apps/front-end/src/app/providers/router/ui/AppRouter.tsx
+++ apps/front-end/src/app/providers/router/ui/AppRouter.tsx
@@ -3,12 +3,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { FinishPage } from '@hackathon-2023/client/pages/finish'
 import { GamePage } from '@hackathon-2023/client/pages/game'
 import { StartPage } from '@hackathon-2023/client/pages/main'
+import { PrehistoryPage } from '@hackathon-2023/client/pages/prehistory'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <StartPage />,
     // TODO: Добавить error boundary
+  },
+  {
+    path: '/prehistory',
+    element: <PrehistoryPage />,
   },
   {
     path: '/game',
